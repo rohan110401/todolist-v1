ce9cd224a0e6c84ac53d43e68dd92223e01895c8
rohan110401 <82100968+rohan110401@users.noreply.github.com>
2022-03-17 22:09:38 +0530

    Add files via upload
    Todo List v-1

---
+++ date.js
@@ -0,0 +1,36 @@
+//jshint esversion:6
+
+exports.getDate = getDate;
+
+function getDate() {
+
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+
+}
+
+exports.getDay = getDay;
+
+function getDay() {
+    
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+
+}
